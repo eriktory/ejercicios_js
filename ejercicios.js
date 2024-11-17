a7f691113fc8bef0b432dc8db715bd49dd9530af
Erik Tory <114362496+eriktory@users.noreply.github.com>
2024-11-17 13:11:18 -0300

    Create ejercicios.js

---
+++ ejercicios.js
@@ -0,0 +1,20 @@
+// ejercicios.js
+const ejercicios = {
+  
+  //ejercicio 01
+  //contarCadena: (cadena="") => { (!cadena) ? console.log(`no hay nada escrito`) : console.log(`la cadena ${cadena}, tiene ${cadena.length} caracteres`)},
+  
+  //ejercico 02
+  //cortarCadena: (cadena="", longitud=undefined) => { (!cadena) ? console.log(`no hay nada escrito`) : console.log(cadena.slice(0,longitud))},
+
+  // Ejercicio 03
+  
+  prueba: () => {
+  console.error('error'),
+  console.warn('alerta'),
+  console.log(`log`),
+  console.info('info')}
+  
+}
+
+module.exports = ejercicios;
