325d11f4bad48eebd0004881ce97a8fba786c642
Erik Tory <114362496+eriktory@users.noreply.github.com>
2024-11-17 13:10:31 -0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,10 @@
+// index.js
+const ejercicios = require('./ejercicios.js');
+
+// Ejercicio 01
+//ejercicios.contarCadena(`hola mundo`); //Imprime 10
+
+// Ejercicio 02
+//ejercicios.cortarCadena(`hola mundo`,4); //Imprime "hola"
+
+ejercicios.prueba()
